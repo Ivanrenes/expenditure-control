2fcce1e270dfde82b119fe126993a3ccf7ea8777
Ivan Rodriguez <ivanrodriguez@MacBook-Pro-de-Ivan-2.local>
2022-02-06 11:48:49 -0500

    feat: css added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     'plugin:react/recommended',
     'airbnb',
     'plugin:jsx-a11y/recommended',
     'plugin:react-hooks/recommended',
-    'prettier',
+    'prettier'
   ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', 'react-hooks', 'prettier'],
   rules: {
@@ -24,20 +24,20 @@ module.exports = {
       'error',
       {
         required: {
-          some: ['nesting', 'id'],
-        },
-      },
+          some: ['nesting', 'id']
+        }
+      }
     ],
     'jsx-a11y/label-has-for': [
       'error',
       {
         required: {
-          some: ['nesting', 'id'],
-        },
-      },
+          some: ['nesting', 'id']
+        }
+      }
     ],
     'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }]
   },
-  ignorePatterns: ['*.svg'],
+  ignorePatterns: ['*.svg', 'src/img/*']
 };

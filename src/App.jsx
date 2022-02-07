940416e128bddf78a631c86a86ef5bee5383e99c
Ivan Rodriguez <ivanrodriguez@MacBook-Pro-de-Ivan-2.local>
2022-02-07 10:38:59 -0500

    feat: header and new budget component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
+import NewBudget from './components/NewBudget';
 
 function App() {
-  return <p>Expenditure Control</p>;
+  return <NewBudget />;
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Component() {
+  return (
+    <header>
+      <h1>Expenditure Control</h1>
+    </header>
+  );
+}
+
+export default Component;
